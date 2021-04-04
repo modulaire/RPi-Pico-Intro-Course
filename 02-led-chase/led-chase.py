39aed401748e06342c6737d4967a2f34af4f3ad2
jasoncook-io <guineabot@riseup.net>
2021-04-04 19:46:50 +0200

    update code and readme

--- 02-led-chase/led-chase.py
+++ 02-led-chase/led-chase.py
@@ -1,25 +1,25 @@
 from machine import Pin
 from utime import sleep
 
+#create a variable for each LED
+#assign each LED a different Pin number and designate it as output
 ledA = Pin(15, Pin.OUT)
 ledB = Pin(14, Pin.OUT)
 ledC = Pin(13, Pin.OUT)
 
-myDelay = 0.2 # myDelay is half of a second
-button = Pin(11, Pin.IN)
+myDelay = 0.5 # myDelay is half of a second
 
 while True:
-    if button.value() == 1:
-        for x in range(10):
-            #turn on ledA and wait half a second
-            ledA.value(1)
-            sleep(myDelay)
-            ledA.value(0)
-            #turn off ledA then turn on ledB and wait half a second
-            ledB.value(1)
-            sleep(myDelay)
-            ledB.value(0)
-            #turn off ledB then turn on ledC for wait a second
-            ledC.value(1)
-            sleep(myDelay)
-            ledC.value(0)
+    #turn on ledA and wait half a second
+    ledA.value(1)
+    sleep(myDelay)
+    ledA.value(0)
+    #turn off ledA then turn on ledB and wait half a second
+    ledB.value(1)
+    sleep(myDelay)
+    ledB.value(0)
+    #turn off ledB then turn on ledC for wait a second
+    ledC.value(1)
+    sleep(myDelay)
+    ledC.value(0)
+    #turn off ledC and return to the beginning of the loop
